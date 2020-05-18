8c2bbcf46afabd8d3ce21bb12debfbe6b0da9817
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-18 17:10:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,9 @@
 class Triangle
+  
   def inititalize(length1, length2, length3)
-    @length1 
-    @length2
-    @length3
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
   end
   
   def kind 
