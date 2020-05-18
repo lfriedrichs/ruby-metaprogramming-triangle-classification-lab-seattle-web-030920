35d7d52d555f223d721dc689d753ef6b6b6ac7c1
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-18 17:05:39 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,12 @@ class Triangle
     if (@length1 == @length2 && @length2 == @length3)
       return :equilateral 
     elsif (@length1 == @length2 || @length2 == @length3 || @length3 == @length1) 
-      return :iso
+      return :isosceles 
+    else 
+      return :scalene
   end 
+  
+  def is_triangle
+    test1 = (@length1 + @length2) > @length3
   
 end
