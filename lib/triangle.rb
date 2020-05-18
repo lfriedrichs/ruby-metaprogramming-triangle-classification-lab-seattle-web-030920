d46aebfc20563a45df4f7855516ff56c488820c9
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-18 17:15:19 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
-  # write code here
+  
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+  
+  def kind 
+    if (is_triangle)
+      if (@length1 == @length2 && @length2 == @length3)
+        return :equilateral 
+      elsif (@length1 == @length2 || @length2 == @length3 || @length3 == @length1)
+        return :isosceles 
+      else 
+        return :scalene
+      end 
+  else 
+    raise TriangleError
+  end
+end
+  
+  def is_triangle
+    test3 = (@length1 + @length2) > @length3
+    test2 = (@length1 + @length3) > @length2
+    test1 = (@length3 + @length2) > @length1
+    if (test1 && test2 && test3)
+      return true 
+    else 
+      return false 
+    end
+  end
+  
+  class TriangleError < StandardError
+    puts "Not a triangle"
+  end
+  
 end
