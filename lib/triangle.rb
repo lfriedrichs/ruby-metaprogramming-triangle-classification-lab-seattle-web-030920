93fa2566d4068e3347ab4fc3b3f2ce7bed36e751
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-05-18 17:07:04 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,13 +7,17 @@ class Triangle
   
   def kind 
     if (is_triangle) do
-      if (@length1 == @length2 && @length2 == @length3)
+      if (@length1 == @length2 && @length2 == @length3) do
         return :equilateral 
-      elsif (@length1 == @length2 || @length2 == @length3 || @length3 == @length1) 
+      elsif (@length1 == @length2 || @length2 == @length3 || @length3 == @length1) do
         return :isosceles 
       else 
         return :scalene
     end 
+  end
+  else do 
+    #customerror
+  end
   
   def is_triangle
     test3 = (@length1 + @length2) > @length3
